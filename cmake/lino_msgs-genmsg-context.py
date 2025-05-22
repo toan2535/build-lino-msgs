78938a5d21072ebd6d052123a322a226d9449fa2
toan2535 <nguyengiatoan2004@gmail.com>
2025-05-21 21:57:46 -0700

    Add files via upload

---
+++ cmake/lino_msgs-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/tiec/Desktop/lino_ws/src/lino_msgs/msg/Velocities.msg;/home/tiec/Desktop/lino_ws/src/lino_msgs/msg/PID.msg;/home/tiec/Desktop/lino_ws/src/lino_msgs/msg/Imu.msg"
+services_str = ""
+pkg_name = "lino_msgs"
+dependencies_str = "std_msgs;geometry_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "lino_msgs;/home/tiec/Desktop/lino_ws/src/lino_msgs/msg;std_msgs;/opt/ros/noetic/share/std_msgs/cmake/../msg;geometry_msgs;/opt/ros/noetic/share/geometry_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python3"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/noetic/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
